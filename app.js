d680bffa26f40962f7a4044dd933e37f58a731a7
bogdan shulika <bsshul@gmail.com>
2024-06-09 20:59:21 +0200

    New text

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.get("/about", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+  console.log(`✅ All is ok! Server is running on port ${PORT}`);
 });
